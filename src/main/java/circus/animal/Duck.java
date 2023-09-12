0b5da41543e68cc3f5545498a83d245b3913b24c
Dexter Hoon <e0727166@u.nus.edu>
2023-09-12 20:48:15 +0800

    Update code according to lecture instructions

--- src/main/java/circus/animal/Duck.java
+++ src/main/java/circus/animal/Duck.java
@@ -1,15 +1,21 @@
+package circus.animal;
+
 public class Duck extends Bird {
     @Override
     public String speak() {
         return "Quack Quack";
     }
 
-    @Override
+    @Override //it is just for annotation
     public String toString() {
         return "I'm a Duck";
     }
 
     public void swim() {
         System.out.println("I'm swimming...");
+    }
+
+    public int getValue() {
+        return 10;
     }
 }
