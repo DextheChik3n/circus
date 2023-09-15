5b8452a05213e7a6d867181c54642fa1626375eb
Dexter Hoon <e0727166@u.nus.edu>
2023-09-15 17:23:28 +0800

    Update circus and animal classes

--- src/main/java/circus/Circus.java
+++ src/main/java/circus/Circus.java
@@ -1,11 +1,12 @@
 package circus;
 
-import circus.animal.Animal;
-import circus.animal.Duck;
-import circus.animal.Parrot;
+import circus.animal.*;
 import circus.stuff.Cannon;
 import circus.stuff.Equipment;
 import circus.stuff.Ladder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Circus {
     private static Animal[] animals = {
@@ -39,9 +40,39 @@ public class Circus {
     }
 
     public static void main(String[] args) {
-        makeAnimalsTalk();
+        System.out.println("no. of animals in the array = " + animals.length);
+//        animals[2] = new Elephant("DumboOne");
+        ArrayList<Animal> animalArrayList = new ArrayList<>(Arrays.asList(animals));
+        System.out.println("size of animal array list = " + animalArrayList.size());
+        System.out.println("no. of animals in the array = " + animals.length);
+
+        Elephant urmom = new Elephant("Urmom"); //index = 2
+        animalArrayList.add(urmom);
+        Tiger teeger = new Tiger("Teeger"); //index = 3
+        Duck dock = new Duck("Dock"); //index = 4
+        Duck quacks = new Duck("Quacks"); //index = 5
+        animalArrayList.add(teeger);
+        animalArrayList.add(dock);
+        animalArrayList.add(quacks);
+
+        System.out.println("size of animal array list = " + animalArrayList.size());
+
+        System.out.println("Before sorting...");
+        for (Animal animal : animalArrayList) {
+            System.out.println(animal);
+        }
+
+        System.out.println("Teeger is at array index = " + animalArrayList.indexOf(teeger));
+
+        animalArrayList.sort(Animal.AnimalNameComparator);
+        System.out.println("After sorting...");
+        printAllAnimals(animalArrayList);
+
+        /*makeAnimalsTalk();
         System.out.println("Total value of animals " + calculateAssetValue(animals));
-        System.out.println("Total value of equipments " + calculateAssetValue(equipments));
+        System.out.println("Total value of equipments " + calculateAssetValue(equipments));*/
+    }
+
     public static void printAllAnimals (ArrayList<Animal> arrayList) {
         for (Animal animal : arrayList) {
             System.out.println(animal);
